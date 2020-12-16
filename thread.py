0c16a163681f4e75d147384edfd0031bcbb33bcd
rinat <kakashi-hatake-win@yandex.ru>
2020-12-16 23:09:33 +0500

    add easy thread

---
+++ thread.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 16 22:35:13 2020
+
+@author: rinat
+"""
+
+import logging
+import threading
+import time
+
+def thread_function(name):
+    logging.info("Thread %s: starting", name)
+    time.sleep(2)
+    logging.info("Thread %s: finishing", name)
+    
+if __name__ == "__main__":
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+    logging.info("Main    : before creating thread")
+    x = threading.Thread(target=thread_function, args=(1,))
+    y = threading.Thread(target=thread_function, args=(2,))
+    logging.info("Main    : before running thread")
+    x.start()
+    time.sleep(5)
+    y.start()
+    logging.info("Main    : wait for the thread to finish")
+    # x.join()
+    logging.info("Main    : all done")
